d36ac1fc4c7351e6d9b48e48f2091ae20b7befd8
Gusti <gustiranda3013@gmail.com>
2023-05-22 17:05:49 +0800

    :lipstick: Update Background Vector

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './assets/scss/style.scss'
+import OrangeVector from "./assets/images/vectors/OrangeVector.svg";
+import OrangeVectorGlow from "./assets/images/vectors/OrangeVectorGlow.svg";
 import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-   <div className="app">
+   <div className="app" style={{ 
+    backgroundImage: `url(${OrangeVector})`,
+    width: '100vw',
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+   }}>
+    <div className="glow-bg" style={{ 
+    backgroundImage: `url(${OrangeVectorGlow})`,
+    width: '100vw',
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+   }}>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage/>} />
         </Routes>
       </Router>
+      </div>
    </div>
   )
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,17 +1,11 @@
 import "../../assets/scss/homestyle.scss";
-import OrangeVector from "../../assets/images/vectors/OrangeVector.svg";
 import { Component } from "react";
 
 class HomePage extends Component {
   render() {
     return (
       <>
-        <img
-          src={OrangeVector}
-          alt=""
-          srcSet=""
-          style={{ objectFit: "contain", width: "100%", height: "100%" }}
-        />
+
       </>
     );
   }
